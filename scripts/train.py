2ca001f3a2940b56a7922047b5d0177353c0e3fa
justinau1145 <147450987+justinau1145@users.noreply.github.com>
2025-12-29 02:01:56 -0800

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import os
-import argeparse
+import argparse
 
 # Project imports
 from models.encoder import VGGEncoder
