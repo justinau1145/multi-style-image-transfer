ae5a25f419c3262c0882ac5ca09367813b30c080
justinau1145 <justinau1145@gmail.com>
2026-01-02 17:14:45 -0800

    test commit

--- core/losses.py
+++ core/losses.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from core.adain_segment import calc_mean_std, adaptive_instance_normalization
 import torch.nn as nn
+#hello
 
 def content_loss(output_features: torch.Tensor, 
                  target_features: torch.Tensor) -> torch.Tensor:
